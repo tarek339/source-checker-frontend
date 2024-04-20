067da75f2ff1ef0b2622c32e838c77a7a8f7530c
Tarek Jassine <tarekjassine@gmail.com>
2024-04-20 18:11:28 +0300

    translation for input error messages

--- src/hooks/useInputErrors.tsx
+++ src/hooks/useInputErrors.tsx
@@ -4,10 +4,10 @@ const useInputErrors = () => {
   const { t } = useTranslations();
 
   const emptyInput = <div className="input-error">{t("input.inputError")}</div>;
-  const incorrectType = <div className="input-error">incorrect type</div>;
-  const fetchError = (
-    <div className="input-error">Falsche ID oder falscher PIN</div>
+  const incorrectType = (
+    <div className="input-error">{t("input.incorrectType")}</div>
   );
+  const fetchError = <div className="input-error">{t("input.fetchError")}</div>;
 
   return {
     emptyInput,
