0e50510f0d307ce26a3319c1d7e67113798789bc
Shahmir Faisal <shahmir049@gmail.com>
2024-04-24 22:32:33 +0500

    added an image in the Authentication component

--- src/views/Authentication.tsx
+++ src/views/Authentication.tsx
@@ -1,36 +1,49 @@
-import { useNavigate } from "react-router-dom";
-import { Footer } from "../components";
-import { useLocaleStorage, useTranslations } from "../hooks";
-import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+import { Footer } from "../components"
+import { useLocaleStorage, useTranslations } from "../hooks"
+import { useEffect } from "react"
+import imgSrc from "../assets/images/charisse-kenion-cJkVMAKDYl0-unsplash.jpg"
 
 const Authentication = () => {
-  const navigate = useNavigate();
-  const { t } = useTranslations();
-  const { resetStates } = useLocaleStorage();
+  const navigate = useNavigate()
+  const { t } = useTranslations()
+  const { resetStates } = useLocaleStorage()
 
   useEffect(() => {
-    resetStates();
-  }, []);
+    resetStates()
+  }, [])
 
   return (
     <div className="authentication">
-      <div className="auth-section">
-        <h1>{t("common.sourceChecker")}</h1>
-        <h1 style={{ color: "#16a34a" }}>{t("auth.question")}</h1>
+      <div className="authentication-container">
+        <div className="auth-section-container">
+          <div className="auth-section">
+            <h1>{t("common.sourceChecker")}</h1>
+            <h1 style={{ color: "#16a34a" }}>{t("auth.question")}</h1>
 
-        <div className="auth-buttons">
-          <button className="teacher-button">{t("auth.studentButton")}</button>
-          <button
-            className="student-button"
-            onClick={() => navigate("/surveys-manager")}>
-            {t("auth.teacherButton")}
-          </button>
+            <div className="auth-buttons">
+              <button className="teacher-button">
+                {t("auth.studentButton")}
+              </button>
+              <button
+                className="student-button"
+                onClick={() => navigate("/surveys-manager")}
+              >
+                {t("auth.teacherButton")}
+              </button>
+            </div>
+            <p>{t("auth.mainText")}</p>
+          </div>
         </div>
-        <p>{t("auth.mainText")}</p>
+
+        <div className="authentication-image">
+          <img src={imgSrc} />
+        </div>
       </div>
+
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Authentication;
+export default Authentication
