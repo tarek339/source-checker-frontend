9c3016d04a18202bc7b17f3a3e4cc3ec49f91912
Tarek Jassine <tarekjassine@gmail.com>
2024-06-09 22:36:16 +0200

    fixed error modal

--- src/App.tsx
+++ src/App.tsx
@@ -18,28 +18,30 @@ function App() {
   const { t } = useTranslations();
   const { mainPage } = useSelectors();
   return (
-    <AnimatePresence mode="wait">
+    <>
       {mainPage ? null : <Header title={t("common.sourceChecker")} />}
-      <Routes>
-        <Route path="/" element={<Authentication />} />
-        <Route
-          path="/surveys-manager/choose-action"
-          element={<ChooseAction />}
-        />
-        <Route
-          path="/surveys-manager/choose-survey"
-          element={<ChooseSurvey />}
-        />
-        <Route path="/surveys-manager/new-survey" element={<NewSurvey />} />
-        <Route
-          path="/surveys-manager/save-survey/:id"
-          element={<SaveSurvey />}
-        />
-        <Route path="/surveys-manager/add-pages/:id" element={<AddPages />} />
-        <Route path="/survey-summary/:id" element={<SurveySummary />} />
-        <Route path="/student-auth" element={<StudenAuth />} />
-      </Routes>
-    </AnimatePresence>
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Authentication />} />
+          <Route
+            path="/surveys-manager/choose-action"
+            element={<ChooseAction />}
+          />
+          <Route
+            path="/surveys-manager/choose-survey"
+            element={<ChooseSurvey />}
+          />
+          <Route path="/surveys-manager/new-survey" element={<NewSurvey />} />
+          <Route
+            path="/surveys-manager/save-survey/:id"
+            element={<SaveSurvey />}
+          />
+          <Route path="/surveys-manager/add-pages/:id" element={<AddPages />} />
+          <Route path="/survey-summary/:id" element={<SurveySummary />} />
+          <Route path="/student-auth" element={<StudenAuth />} />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
--- src/components/surveys-manager/new-survey/ErrorModal.tsx
+++ src/components/surveys-manager/new-survey/ErrorModal.tsx
@@ -1,4 +1,9 @@
-import { useDispatches, useSelectors, useTranslations } from "../../../hooks";
+import {
+  useBreakPoints,
+  useDispatches,
+  useSelectors,
+  useTranslations,
+} from "../../../hooks";
 import { IErrorModal } from "../../../types/interfaces/components";
 import CancelButton from "../../buttons/CancelButton";
 import ContButton from "../../buttons/ContButton";
@@ -13,18 +18,22 @@ const ErrorModal = ({ onSubmit }: IErrorModal) => {
   const { modal } = useSelectors();
   const { t } = useTranslations();
   const { closeModal } = useDispatches();
+  const { windowWidth } = useBreakPoints();
 
   return (
     <Modal isVisible={modal} setIsVisible={closeModal}>
       <Flex direction={"row"} gap={"10px"} align="flex-start">
         <Alert />
         <Flex direction={"column"} gap={"5px"}>
-          <SubHeader title={t("common.error")} />
+          <SubHeader style={{ paddingTop: "3px" }} title={t("common.error")} />
           <p>{t("newSurvey.connectionError")}</p>
           <p>{t("newSurvey.conErrMsgOne")}</p>
           <p>{t("newSurvey.conErrMsgTwo")}</p>
           <FormContainer
-            style={{ paddingTop: "10px", alignItems: "flex-end" }}
+            style={{
+              paddingTop: "10px",
+              alignItems: windowWidth <= 500 ? "" : "flex-end",
+            }}
             onSubmit={onSubmit}
             gap={"0px"}>
             <ButtonContainer>
