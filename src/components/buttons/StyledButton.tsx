fa80e8fd85d0ecb9de536083a06acaf122517770
Tarek Jassine <tarekjassine@gmail.com>
2024-08-30 21:31:19 +0200

    fixed mui bug

--- src/components/buttons/StyledButton.tsx
+++ src/components/buttons/StyledButton.tsx
@@ -3,39 +3,49 @@ import styled from "styled-components";
 import { useSelectors } from "../../hooks";
 import Flex from "../containers/Flex";
 import { IButton } from "../../types/interfaces/components";
+import { useState } from "react";
+
+const StyledMUIButton = styled(Button)`
+  && {
+    color: #fff;
+    padding: 6px 20px;
+    height: 37px;
+    border-radius: 45px;
+    font-weight: bold;
+    transition: background-color 0.3s;
+    font-size: 18px;
+    text-transform: uppercase;
+    font-weight: 600;
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px,
+      rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
+
+    &:disabled {
+      background-color: #ccc;
+      color: #777;
+    }
+  }
+`;
 
 const StyledButton = ({ title, type, error, icon, gap, onClick }: IButton) => {
+  const [hovered, setHovered] = useState(false);
   const { loading } = useSelectors();
 
-  const StyledMUIButton = styled(Button)`
-    && {
-      background-color: ${error ? "#ff0000" : "#2835c3"};
-      color: #fff;
-      padding: 6px 20px;
-      height: 37px;
-      cursor: ${loading ? "default" : "pointer"};
-      border-radius: 45px;
-      font-weight: bold;
-      transition: background-color 0.3s;
-      font-size: 18px;
-      text-transform: uppercase;
-      font-weight: 600;
-      box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px,
-        rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
-
-      &:hover {
-        background-color: ${error ? "#cc0000" : "#1420ae"};
-      }
-
-      &:disabled {
-        background-color: #ccc;
-        color: #777;
-      }
-    }
-  `;
-
   return (
-    <StyledMUIButton type={!type ? "button" : type} onClick={onClick}>
+    <StyledMUIButton
+      style={{
+        backgroundColor: error
+          ? "#ff0000"
+          : error && hovered
+          ? "#cc0000"
+          : hovered
+          ? "#1420ae"
+          : "#2835c3",
+        cursor: loading ? "default" : "pointer",
+      }}
+      type={!type ? "button" : type}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onClick={onClick}>
       <Flex direction={"row"} gap={gap!} align="center">
         <>
           {icon}
