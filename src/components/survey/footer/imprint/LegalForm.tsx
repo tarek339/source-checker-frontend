8852ff2f05e3b9415bde08bf6b3bb43dfadf8ef2
Tarek Jassine <tarekjassine@gmail.com>
2024-09-01 02:00:23 +0200

    optimized footer

--- src/components/survey/footer/imprint/LegalForm.tsx
+++ src/components/survey/footer/imprint/LegalForm.tsx
@@ -3,24 +3,44 @@ import Span from "../../../fonts/Span";
 
 const LegalForm = () => {
   return (
-    <Flex direction={"column"} gap={"10px"}>
-      <Span title={"Rechtsform und gesetzliche Vertretung"} fontSize={18} />
-      <Span
-        title={"Die HAW Hamburg ist eine Körperschaft des Öffentlichen Rechts."}
-      />
-      <Span
-        title={
-          "Sie wird gesetzlich vertreten durch Prof. Dr. Ute Lohrentz, Präsidentin der HAW Hamburg, Berliner Tor 5, 20099 Hamburg."
-        }
-      />
-      <Span title={"Zuständige Aufsichtsbehörde"} fontSize={18} />
-      <Span
-        title={
-          "Behörde für Wissenschaft, Forschung, Gleichstellung und Bezirke der Freien und Hansestadt Hamburg, Hamburger Straße 37, D-22083 Hamburg"
-        }
-      />
-      <Span title={"Umsatzsteueridentifikationsnummer"} fontSize={18} />
-      <Span title={"DE 246571598"} />
+    <Flex direction={"column"} gap={"30px"}>
+      <Flex direction={"column"} gap={"10px"}>
+        <Span
+          title={"Rechtsform und gesetzliche Vertretung"}
+          fontSize={20}
+          fontWeight={600}
+        />
+        <Span
+          title={
+            "Die HAW Hamburg ist eine Körperschaft des Öffentlichen Rechts."
+          }
+        />
+        <Span
+          title={
+            "Sie wird gesetzlich vertreten durch Prof. Dr. Ute Lohrentz, Präsidentin der HAW Hamburg, Berliner Tor 5, 20099 Hamburg."
+          }
+        />
+      </Flex>
+      <Flex direction={"column"} gap={"10px"}>
+        <Span
+          title={"Zuständige Aufsichtsbehörde"}
+          fontSize={20}
+          fontWeight={600}
+        />
+        <Span
+          title={
+            "Behörde für Wissenschaft, Forschung, Gleichstellung und Bezirke der Freien und Hansestadt Hamburg, Hamburger Straße 37, D-22083 Hamburg"
+          }
+        />
+      </Flex>
+      <Flex direction={"column"} gap={"10px"}>
+        <Span
+          title={"Umsatzsteueridentifikationsnummer"}
+          fontSize={20}
+          fontWeight={600}
+        />
+        <Span title={"DE 246571598"} />
+      </Flex>
     </Flex>
   );
 };
