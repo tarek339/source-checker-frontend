d9df7c86398b211d391947414123091b6e899721
Tarek Jassine <tarekjassine@gmail.com>
2024-11-04 19:42:06 +0100

    scrollbar now visible

--- src/components/survey/student-survey/ScreenShotHolder.tsx
+++ src/components/survey/student-survey/ScreenShotHolder.tsx
@@ -1,14 +1,16 @@
 import { IScreenShotHolder } from "../../../types/interfaces/components";
 import Flex from "../../containers/Flex";
-import SubCard from "../../containers/SubCard";
 
 const ScreenShotHolder = ({ src }: IScreenShotHolder) => {
   return (
     <Flex direction={"column"} gap={"20px"}>
-      <SubCard
+      <div
         style={{
           height: "80vh",
           overflowY: "scroll",
+          zIndex: 1,
+          backgroundColor: "#fff",
+          boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
         }}>
         <img
           style={{
@@ -18,7 +20,7 @@ const ScreenShotHolder = ({ src }: IScreenShotHolder) => {
           src={src}
           alt=""
         />
-      </SubCard>
+      </div>
     </Flex>
   );
 };
